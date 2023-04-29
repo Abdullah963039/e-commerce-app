01b2b48164731633d72aecea3786304f617f13bb
AbdullahAlWaraa <103293258+Abdullah963039@users.noreply.github.com>
2023-04-29 18:56:24 +0300

    Finish Create New Product

--- src/Listeners/Product/AdminAddProduct.hook.js
+++ src/Listeners/Product/AdminAddProduct.hook.js
@@ -1,10 +1,191 @@
-import { useRef } from "react";
 import { useStore } from "../../hooks/useStore";
-import { ProductColorsSelectHook } from "./AddProductFormInputs/ProductColorsSelect.hook";
+import { useState, useRef, useEffect } from "react";
+import notify from "../../utils/notifcation";
+
+const colors = [
+  { name: "أحمر", hex: "#F30B0B" },
+  { name: "برتقالي", hex: "#ba4f07" },
+  { name: "اصفر", hex: "#ba9707" },
+  { name: "اخضر", hex: "#44ba07" },
+  { name: "سماوي", hex: "#07ba8e" },
+  { name: "ازرق", hex: "#0753ba" },
+  { name: "بنفسجي", hex: "#5807ba" },
+  { name: "وردي", hex: "#de33a9" },
+  { name: "اسود", hex: "#222" },
+  { name: "ابيض", hex: "#fefefe" },
+];
 
 export default function AdminAddProductHook() {
-  async function sendData(e) {
+  //? Global Store
+  const {
+    subCategories,
+    loading,
+    allCategories: { data: categories },
+    allBrands: { data: brands },
+    getAllCategories,
+    getAllBrands,
+    getAllSubCategoriesOnCategory,
+    createNewProduct,
+  } = useStore();
+
+  //? Use States
+  const [imageCover, setImageCover] = useState(""); //> Image Cover State
+  const [productImages, setProductImages] = useState([]); //> Product Images State
+  const [mainCategory, setMainCategory] = useState(""); //> Main Category State
+  const [budges, setBudges] = useState([]); //> Selected Subcategories State
+  const [selectedBrand, setSelectedBrand] = useState(""); //> Selected Subcategories State
+  const [availColors, setAvailColors] = useState([]); //> Available Colors State
+
+  //? Use Ref
+  const nameRef = useRef("");
+  const descriptionRef = useRef("");
+  const quantityRef = useRef("");
+  const priceRef = useRef("");
+  const soldPriceRef = useRef("");
+
+  // ? Product Image Cover
+  const selectImageCover = (e) => {
+    if (e.target.files[0] === undefined) return;
+    setImageCover(e.target.files[0]);
+  };
+  const deleteImageCover = () => setImageCover(""); //* Delete image cover
+
+  // ? Product Images
+  const selectMultipleImages = (e) => {
+    if (e.target.files[0] === undefined) return;
+    if (productImages.length > 3) {
+      notify("error", "لا يمكن اضافة أكتر من 4 صور");
+      return;
+    }
+    setProductImages((prevImages) => [...prevImages, ...e.target.files]);
+  };
+
+  const deleteProductImage = (image) =>
+    setProductImages([...productImages.filter((images) => images !== image)]);
+
+  //? Category & Sub Category & Brand
+  const addSubCategory = (e) => setBudges(e.value); //> Add Subcategory id Subcategories State
+
+  const selectMainCategory = (e) => setMainCategory(e.value);
+
+  const selectNewBrand = (e) => setSelectedBrand(e.value);
+
+  //? Available Colors
+  const addColor = (hex) => setAvailColors((prev) => [...prev, hex]); //> Add Color
+  const deleteColor = (hex) =>
+    setAvailColors([...availColors.filter((clr) => clr !== hex)]); //> Delete Color
+
+  //? Use Effect
+  useEffect(() => {
+    getAllCategories();
+    getAllBrands();
+  }, []);
+
+  useEffect(() => {
+    if (mainCategory !== "") getAllSubCategoriesOnCategory(mainCategory);
+  }, [mainCategory]);
+
+  //? Handle Submit Function
+  const sendData = async (e) => {
     e.preventDefault();
-  }
-  return { formRef, sendData };
+
+    // > Form Validation
+    if (
+      nameRef.current.value === "" ||
+      descriptionRef.current.value === "" ||
+      quantityRef.current.value === "" ||
+      priceRef.current.value === "" ||
+      mainCategory === "" ||
+      //! selectedBrand === "" ||
+      availColors.length === 0 ||
+      imageCover === "" ||
+      productImages.length === 0
+    ) {
+      notify("error", "اكمل البيانات من فضلك !");
+      return;
+    }
+
+    const formData = new FormData(e.target);
+
+    //> Add Product Images
+    for (let i = 0; i < productImages.length; i++) {
+      formData.append("images", productImages[i]);
+    }
+
+    formData.append("category", mainCategory); //> Main Category
+    //! formData.append("brand", selectedBrand);
+
+    //> Add Subcategories If It Exist
+    if (budges.length > 0) {
+      for (let i = 0; i < budges.length; i++) {
+        formData.append("subcategory", budges[i]);
+      }
+    }
+
+    for (let i = 0; i < availColors.length; i++) {
+      formData.append("availableColors", availColors[i]);
+    }
+
+    const response = await createNewProduct(formData);
+
+    console.log(response);
+
+    if (response.status === 400) notify("error", "هذا الاسم موجود مسبقا");
+    if (response.status === 201) {
+      notify("done");
+      clearAllData();
+    }
+  };
+
+  // > Clear All Data After Success Submit Data
+  const clearAllData = () => {
+    setImageCover("");
+    setProductImages([]);
+    setMainCategory("");
+    setBudges([]);
+    setSelectedBrand("");
+    setAvailColors([]);
+
+    nameRef.current.value === "";
+    descriptionRef.current.value === "";
+    quantityRef.current.value === "";
+    priceRef.current.value === "";
+  };
+
+  return {
+    sendData,
+    loading,
+    imageCoverLogic: { imageCover, selectImageCover, deleteImageCover },
+    productImagesLogic: {
+      productImages,
+      selectMultipleImages,
+      deleteProductImage,
+    },
+    nameRef,
+    descriptionRef,
+    quantityRef,
+    priceRef,
+    soldPriceRef,
+    subCategoriesLogic: {
+      budges,
+      subCategories,
+      addSubCategory,
+    },
+    MainCategoryLogic: {
+      categories,
+      mainCategory,
+      selectMainCategory,
+    },
+    brandsLogic: {
+      brands,
+      selectedBrand,
+      selectNewBrand,
+    },
+    colorsLogic: {
+      colors,
+      addColor,
+      deleteColor,
+      availColors,
+    },
+  };
 }
--- src/components/Admin/AdminAddProduct.jsx
+++ src/components/Admin/AdminAddProduct.jsx
@@ -1,109 +1,293 @@
+import { LoadingIcon } from "../Utility/Loading";
+import { MultiSelect } from "primereact/multiselect";
+import { Dropdown } from "primereact/dropdown";
+// Styles
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+// Icons
 import { MdDriveFolderUpload } from "react-icons/md";
 import { AiOutlineClose } from "react-icons/ai";
 import AdminAddProductHook from "../../Listeners/Product/AdminAddProduct.hook";
+// Notification
 import { ToastContainer } from "react-toastify";
-import {
-  BrandSelect,
-  MainCategorySelect,
-  SubCategorySelect,
-} from "./AddProductFormInputs/SelectBox";
-import ProductColorsSelect from "./AddProductFormInputs/ProductColorsSelect";
-import AddImageCover from "./AddProductFormInputs/AddImageCover";
-import AddProductImages from "./AddProductFormInputs/AddProductImages";
-import {
-  ProductDescription,
-  ProductPrice,
-  ProductQuantity,
-  ProductTitle,
-} from "./AddProductFormInputs/ProductInfoInputs";
 
 export default function AdminAddProduct() {
   const {
-    sendData, //> Handle Submit Method
-    formRef,
+    sendData,
+    loading,
+    imageCoverLogic: { imageCover, selectImageCover, deleteImageCover },
+    productImagesLogic: {
+      productImages,
+      selectMultipleImages,
+      deleteProductImage,
+    },
+    nameRef,
+    descriptionRef,
+    quantityRef,
+    priceRef,
+    soldPriceRef,
+    subCategoriesLogic: { budges, subCategories, addSubCategory },
+    MainCategoryLogic: { categories, mainCategory, selectMainCategory },
+    brandsLogic: { brands, selectedBrand, selectNewBrand },
+    colorsLogic: { colors, addColor, deleteColor, availColors },
   } = AdminAddProductHook();
 
   return (
     <>
       <form
-        ref={formRef}
         onSubmit={sendData}
         id="newProduct"
         className="flex max-w-[600px] flex-col gap-2"
       >
         {/* Add Product Image Cover */}
 
-        <AddImageCover />
+        <>
+          <div className="mb-4 flex w-fit">
+            <input
+              type="file"
+              id="imageCover"
+              name="imageCover"
+              hidden
+              onChange={selectImageCover}
+            />
+            <label
+              htmlFor="imageCover"
+              className="cursor-pointer select-none text-slate-400"
+            >
+              <span className="text-slate-500">صورة غلاف المنتج</span>
+              {imageCover === "" ? (
+                <MdDriveFolderUpload className="text-9xl duration-150 hover:text-slate-700" />
+              ) : (
+                <>
+                  <img
+                    src={URL.createObjectURL(imageCover)}
+                    alt="Product Cover"
+                    className="my-2 w-36"
+                  />
+                </>
+              )}
+            </label>
+            {
+              //>Delete Image Cover
+              imageCover && (
+                <>
+                  <button
+                    onClick={deleteImageCover}
+                    type="button"
+                    className="btn icon mx-4 w-fit self-end bg-slate-800 px-2 py-1 text-white hover:text-red-500"
+                  >
+                    <AiOutlineClose />
+                    مسح
+                  </button>
+                </>
+              )
+            }
+          </div>
+        </>
 
         {/* Product Images */}
-        <AddProductImages />
+        <>
+          <div className="flex flex-wrap items-center gap-2">
+            {productImages.length < 4 && (
+              <>
+                <label className="cursor-pointer select-none self-start text-slate-400">
+                  <input
+                    type="file"
+                    multiple
+                    hidden
+                    onChange={selectMultipleImages}
+                  />
+                  <span className="text-slate-500">صور المنتج</span>
+                  <MdDriveFolderUpload className="text-9xl duration-150 hover:text-slate-700" />
+                </label>
+              </>
+            )}
+            {productImages.length > 0 && (
+              <div className="grid w-full grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4">
+                {productImages.map((image, index) => (
+                  <div
+                    key={index}
+                    className="relative col-span-1 flex w-fit items-center rounded-md bg-slate-100 p-2"
+                  >
+                    <img
+                      src={URL.createObjectURL(image)}
+                      alt="Product Image"
+                      className="w-32"
+                    />
+                    <AiOutlineClose
+                      className="absolute left-1 top-1 cursor-pointer self-start hover:text-red-500"
+                      onClick={() => deleteProductImage(image)}
+                    />
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </>
 
         {/* Product Name */}
-        <ProductTitle />
+        <>
+          <input
+            type="text"
+            placeholder="اسم المنتج"
+            title="اسم المنتج"
+            name="title"
+            ref={nameRef}
+            className="w-full rounded-md border border-slate-300 p-2 outline-none"
+          />
+        </>
 
         {/* Product Description */}
-        <ProductDescription />
+        <>
+          <textarea
+            name="description"
+            rows="3"
+            className="min-h-[50px] w-full resize-y rounded-md border border-slate-300 p-2 outline-none"
+            placeholder="وصف المنتج"
+            title="وصف المنتج"
+            ref={descriptionRef}
+          ></textarea>
+        </>
 
         {/* Product Quantity */}
-        <ProductQuantity />
+        <>
+          <input
+            ref={quantityRef}
+            name="quantity"
+            type="number"
+            min={1}
+            placeholder="الكمية المتاحة"
+            title="الكمية المتاحة"
+            className="w-full rounded-md border border-slate-300 p-2 outline-none invalid:outline-2 invalid:outline-red-500"
+          />
+        </>
 
         {/* Product Price */}
-        <ProductPrice />
+        <>
+          <input
+            name="price"
+            ref={priceRef}
+            min={0}
+            type="number"
+            placeholder="سعر المنتج"
+            title="سعر المنتج"
+            className="w-full rounded-md border border-slate-300 p-2 outline-none"
+          />
+        </>
+
+        {/* Sold Price */}
+        <>
+          <input
+            name="sold"
+            ref={soldPriceRef}
+            min={0}
+            type="number"
+            placeholder="السعر بعد الخصم"
+            title="السعر بعد الخصم"
+            className="w-full rounded-md border border-slate-300 p-2 outline-none"
+          />
+        </>
 
         {/* Product Category */}
-        {/* <>
-          <select
-            name="category"
-            title="التصنيف الرئيسي"
-            className="w-full rounded-md border border-slate-300 bg-white p-2 outline-none"
-            onChange={selectCategory}
-          >
-            <option value="" defaultChecked>
-              التصنيف الرئيسي
-            </option>
-            {categories?.length > 0
-              ? categories?.map((category) => (
-                  <option value={category["_id"]} key={category["_id"]}>
-                    {category["name"]}
-                  </option>
-                ))
-              : "لا يوجد تصنيفات رئيسية"}
-          </select>
-        </> */}
-        <MainCategorySelect />
+
+        <>
+          <Dropdown
+            value={mainCategory}
+            onChange={selectMainCategory}
+            options={categories}
+            optionLabel="name"
+            optionValue="_id"
+            placeholder="التصنيف الرئيسي"
+            className="w-full"
+          />
+        </>
 
         {/* Product Sub Category */}
-        <SubCategorySelect />
+        <>
+          <MultiSelect
+            options={subCategories}
+            display="chip"
+            onChange={addSubCategory}
+            optionLabel="name"
+            value={budges}
+            optionValue="_id"
+            placeholder="اضف تصنيف فرعي"
+            maxSelectedLabels={3}
+            showSelectAll={false}
+            className="w-full"
+          />
+        </>
 
         {/* Product Brand */}
-        <BrandSelect />
-        {/* <>
-          <select
-            name="brand"
-            className="w-full rounded-md border border-slate-300 bg-white p-2 outline-none"
-          >
-            <option value="" defaultChecked>
-              الماركة{" "}
-            </option>
-            {brands?.length > 0
-              ? brands.map((brand) => (
-                  <option value={brand["_id"]} key={brand["_id"]}>
-                    {brand["name"]}
-                  </option>
-                ))
-              : "لا يوجد ماركات"}
-          </select>
-        </> */}
+        <>
+          <Dropdown
+            value={selectedBrand}
+            onChange={selectNewBrand}
+            options={brands}
+            optionLabel="name"
+            optionValue="_id"
+            placeholder="الماركة"
+            className="w-full"
+          />
+        </>
 
         {/* Product Colors */}
-        <ProductColorsSelect />
+        <>
+          <div>
+            <h2 className="my-2 text-slate-700">الالوان المتاحة للمنتج</h2>
+            <div className="flex items-center gap-2">
+              {
+                //> Selected Colors
+                availColors.map((hex, index) => (
+                  <span
+                    key={index}
+                    className="circle cursor-pointer"
+                    onClick={() => deleteColor(hex)}
+                    style={{ backgroundColor: hex }}
+                    title="اضغط للحذف"
+                  ></span>
+                ))
+              }
+              <div className="relative select-none">
+                <input
+                  type="checkbox"
+                  id="colors"
+                  hidden
+                  className="peer/color"
+                />
+                <label
+                  htmlFor="colors"
+                  className="circle flex items-center justify-center bg-slate-100 text-2xl duration-150 peer-checked/color:rotate-45 peer-checked/color:font-bold"
+                >
+                  +
+                </label>
+                <div className="absolute right-full top-0 mx-2 hidden max-h-32 w-64 overflow-y-auto rounded-lg bg-white p-2 peer-checked/color:block">
+                  {colors.map(({ name, hex }, index) => (
+                    <option
+                      key={index}
+                      value={hex}
+                      hidden={availColors.includes(hex)}
+                      onClick={() => addColor(hex)}
+                      className="w-full cursor-pointer p-2 py-1 text-sm hover:bg-slate-200 disabled:cursor-not-allowed disabled:line-through"
+                    >
+                      {name}
+                    </option>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
 
         {/* Send Form Data Button */}
         <button
           type="submit"
-          className="btn w-fit self-end bg-slate-800 px-4 py-2 text-white"
+          className={`btn ${
+            loading && `icon`
+          } w-fit self-end bg-slate-800 px-4 py-2 text-white`}
         >
           حفظ التعديلات
+          {loading && <LoadingIcon className="mr-2" />}
         </button>
       </form>
       <ToastContainer rtl />
